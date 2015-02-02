dcf8c6fee294fdda2064429d314c6c6b309b397a
Jian Shen Tan <jianshentan@Jians-MacBook-Pro.local>
2015-02-02 13:02:07 -0500

    redis

---
+++ README.md
@@ -0,0 +1,14 @@
+# Alive (Server-Side Application)
+
+## Install Instructions
+1. install mongodb
+2. install redis
+3. install express 4.2
+
+## Run Application Instructions
+1. cd into directory
+2. start mongodb: type 'mongod'
+3. start redis: inside redis, type 'src/redis-server'
+4. type "npm install"
+5. type "nodemon bin/www"
+6. (optional) run 'mongo_script.js' for dev
--- sockets/base.js
+++ sockets/base.js
@@ -1,21 +1,16 @@
+var redis = require('redis');
 var Room = require( '../models/room' );
-var rooms = {}; 
+var User = require( '../models/user' );
 
-exports.addRoom = function( roomId ) {
-  rooms[ roomId ] = [];
-};
+var rooms = {}; 
+var redisClient = redis.createClient(); // can specify ( post, host )
+
+redisClient.on( 'connect', function() {
+  console.log( "redis db connected" );
+});
 
 exports.start = function(io) {
   
-  // TODO: consider redis, ram problems...
-  
-  Room.find( {}, function( err, data ) {
-    if( err ) throw err;
-    for( var i in data ) {
-      rooms[ data[i]._id ] = [];
-    }
-  });
-
   io.on( 'connection', function( socket ) { 
     var userId = null;
 
@@ -24,45 +19,52 @@ exports.start = function(io) {
       userId = data.user;
 
       // check that users joins a room (and doesnt make one up)
-      if( rooms[ roomId ] != null ) {
+      redisClient.zrank([ 'rooms', roomId ], function( err, reply ) {
+        if( err ) throw err;
+
+        // if room exists, reply will be an integer between [0, infinity]
+        if( !isInteger( reply ) ) {
+          console.log( "ERROR: room id is not available on redis" );
+          throw err;
+        }
+ 
         socket.join( roomId );
 
-        updateRoom( roomId, userId );
-
-        if( !rooms[ roomId ] ) {
-          rooms[ roomId ] = [ userId ];
-        } else {
-          rooms[ roomId ].push( userId );
-        }
+        // add user to room in mongodb
+        updateRoomEntry( roomId, userId );
+        // add user to room in redis
+        redisClient.zadd([ roomId, Date.now(), userId ], function(){} );
 
         io.to( roomId ).emit( 'user joined', userId );
-        io.to( roomId ).emit( 'user list', rooms[ roomId ] );
-      }
+        // TODO: emit list of users in room to current socket client
+        // io.to( roomId ).emit( 'user list', user._id ); 
+      });
     });
 
     socket.on( 'leave', function( data ) {
       var roomId = data.room;
-      //var userId = data.user;
 
       socket.leave( roomId );
 
-      if( inArray( userId, rooms[ roomId ] ) ) {
-        var userIndex = rooms[ roomId ].indexOf( userId );
-        rooms[ roomId ].splice( userIndex, 1 ); 
-      }
+      // remove user from room in redis
+      redisClient.zrem([ roomId, userId ], function( err, reply ) {
+        io.to( roomId ).emit( 'user left', userId );
+      });
 
-      io.to( roomId ).emit( 'user left', userId );
-      io.to( roomId ).emit( 'user list', rooms[ roomId ] );
     });
     
   });
 }
 
-function updateRoom( roomId, userId ) {
-  // update room in db
+exports.addRoom = function( roomId ) {
+  var score = Date.now();
+  redisClient.zadd([ 'rooms', score, roomId ]);
+};
+
+function updateRoomEntry( roomId, userId ) {
   Room.findById( roomId, function( err, room ) {
     if( err ) throw err;
-    // TODO: check that userId is valid before putting it in db
+    if( !room ) throw err;
     room.users.push( { user: userId, date: Date.now() } );
     room.save( function( err ){
       if( err ) throw err;
@@ -70,14 +72,10 @@ function updateRoom( roomId, userId ) {
   });
 };
 
-function inArray( item, array ) {
-  var inArray = false;
-  for( var i in array ) {
-    if( item == array[i] ) {
-      inArray = true;
-    }
-  }
-  return inArray;
+function isInteger( data ) {
+  if( typeof data==='number' && (data%1)===0 )
+    return true;
+  else
+    return false;
 };
-
 
