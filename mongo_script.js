72231ef817d23bc875bbac863a7a59c805018f6d
Jian Shen Tan <jianshentan@Jians-MacBook-Pro.local>
2015-02-04 00:42:39 -0500

    forgot to encrypt mongo_script user passwords

--- mongo_script.js
+++ mongo_script.js
@@ -3,6 +3,7 @@ var User = require( './models/user' );
 var Room = require( './models/room' );
 var jwt = require( 'jwt-simple' );
 var secret = require( './config/secret' ).app_secret;
+var bcrypt = require( 'bcrypt-nodejs' );
 
 mongoose.connect( 'mongodb://localhost/alive' );
 var db = mongoose.connection;
@@ -31,30 +32,30 @@ db.once( 'open', function callback() {
   var dummyUsers = [];
   var dummyUser01 = new User({
     username: 'js',
-    password: 'tan',
     access_token: jwt.encode( { username: 'js' }, secret )
   });
+  dummyUser01.password = dummyUser01.generateHash( 'tan' );
   dummyUsers.push( dummyUser01 );
 
   var dummyUser02 = new User({
     username: 'minsoo',
-    password: 'thigpen',
     access_token: jwt.encode( { username: 'minsoo' }, secret )
   });
+  dummyUser02.password = dummyUser02.generateHash( 'thigpen' );
   dummyUsers.push( dummyUser02 );
 
   var dummyUser03 = new User({
     username: 'pete',
-    password: 'kim',
     access_token: jwt.encode( { username: 'pete' }, secret )
   });
+  dummyUser03.password = dummyUser03.generateHash( 'kim' );
   dummyUsers.push( dummyUser03 );
 
   var dummyUser04 = new User({
     username: 'cody',
-    password: 'fitzgerald',
     access_token: jwt.encode( { username: 'cody' }, secret )
   });
+  dummyUser04.password = dummyUser04.generateHash( 'fitzgerald' );
   dummyUsers.push( dummyUser04 );
 
   dummyUsers.forEach( function( dummyUser ) {
